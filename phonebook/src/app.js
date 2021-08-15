1a41ee18e63435161ac9b0028956f567044b1a77
Malcolm Macleod <malcolm@macleodhome.com>
2021-08-15 18:24:54 +0100

    Add files before part 3 changes

--- phonebook/src/app.js
+++ phonebook/src/app.js
@@ -1,125 +1,119 @@
-import React, { useState, useEffect } from "react";
-import Filter from "./filter";
-import PersonForm from "./personform";
-import Persons from "./persons";
-import personService from "./services/persons";
-import Notification from "./notification";
-import Error from "./error";
+import React, { useState, useEffect } from 'react'
+import Filter from './filter'
+import PersonForm from './personform'
+import Persons from './persons'
+import personService from './services/persons'
+import Notification from './notification'
+import Error from './error'
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [filter, setFilter] = useState("");
-  const [notification, setNotification] = useState("");
-  const [error, setError] = useState("");
+  const [persons, setPersons] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+  const [ notification, setNotification] = useState('')
+  const [ error, setError] = useState('')
 
   const hook = () => {
-    console.log("effect");
-    personService.getAll().then((returnedPersons) => {
-      console.log("promise fulfilled");
-      setPersons(returnedPersons);
-    });
-  };
+    console.log('effect')
+    personService.getAll()
+      .then(returnedPersons => {
+        console.log('promise fulfilled')
+        setPersons(returnedPersons)
+      })
+  }
 
-  useEffect(hook, []);
+  useEffect(hook, [])
 
   const handleSave = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const exists = persons.find((p) => p.name === newName);
+    const exists = persons.find(p => p.name === newName)
 
     if (exists && exists.number !== newNumber) {
-      if (
-        window.confirm(
-          `${newName} is already added to phonebook, replace the old number with a new one?`
-        )
-      ) {
-        exists.number = newNumber;
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        exists.number = newNumber
 
-        personService
-          .update(exists.id, exists)
-          .then((updated) => {
-            setNotification(`Person '${updated.name}' has a new number`);
-            setTimeout(() => {
-              setNotification(null);
-            }, 5000);
-            personService.getAll().then((returnedPersons) => {
-              console.log("promise fulfilled");
-              setPersons(returnedPersons);
-              setNewName("");
-              setNewNumber("");
-            });
-          })
-          .catch((error) => {
-            setError(
-              `Information of ${exists.name} has already been removed from the server`
-            );
-          });
+        personService.update(exists.id, exists)
+        .then((updated) => {
+          setNotification(
+          `Person '${updated.name}' has a new number`
+          )
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+          personService.getAll()
+          .then(returnedPersons => {
+            console.log('promise fulfilled')
+            setPersons(returnedPersons)
+            setNewName('')
+            setNewNumber('')
+            })
+        })
+         .catch(error => {
+           setError(
+          `Information of ${exists.name} has already been removed from the server`
+          )
+        })
       }
 
-      return;
+      return
     }
 
     if (exists) {
-      alert(`${newName} is already added to phonebook`);
+      alert(`${newName} is already added to phonebook`) 
 
-      return;
+      return
     }
 
     const person = {
       id: newName,
       name: newName,
-      number: newNumber,
-    };
+      number: newNumber
+    }
 
-    personService
-      .create(person)
-      .then((returnedPerson) => {
-        setNotification(`Added '${returnedPerson.name}' to phone book`);
-        setTimeout(() => {
-          setNotification(null);
-        }, 5000);
-        console.log(returnedPerson);
-        setPersons(persons.concat(returnedPerson));
-        setNewName("");
-        setNewNumber("");
-      })
-      .catch((error) => {
-        setError(`Person validation failed: ${error}`);
-      });
-  };
+    personService.create(person)
+    .then(returnedPerson => {
+      setNotification(
+          `Added '${returnedPerson.name}' to phone book`)
+          setTimeout(() => {
+            setNotification(null)
+          }, 5000)
+      console.log(returnedPerson)
+      setPersons(persons.concat(returnedPerson))
+      setNewName('')
+      setNewNumber('')
+    })
+  }
 
   const deletePerson = (person) => {
+    
     if (window.confirm(`Do you want to delete ${person.name}?`)) {
-      console.log("Going to delete person", person.name);
-      personService.deletePerson(person.id).then(() =>
-        personService.getAll().then((returnedPersons) => {
-          console.log("promise fulfilled");
-          setPersons(returnedPersons);
-        })
-      );
+      console.log("Going to delete person", person.name)
+      personService.deletePerson(person.id)
+      .then(() => 
+      personService.getAll()
+      .then(returnedPersons => {
+        console.log('promise fulfilled')
+        setPersons(returnedPersons)
+      })
+      )
     }
-  };
+  }
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    setNewName(event.target.value)
+  }
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+    setNewNumber(event.target.value)
+  }
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
+    setFilter(event.target.value)
+  }
 
-  const filteredPersons =
-    filter.length > 0
-      ? persons.filter((p) =>
-          p.name.toLowerCase().includes(filter.toLowerCase())
-        )
-      : persons;
+  const filteredPersons = filter.length > 0 ? persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase())) : persons
 
   return (
     <div>
@@ -128,17 +122,11 @@ const App = () => {
       <Error message={error} />
       <Filter handleFilterChange={handleFilterChange} filter={filter} />
       <h2>add a new</h2>
-      <PersonForm
-        name={newName}
-        number={newNumber}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
-        handleSave={handleSave}
-      />
+      <PersonForm name={newName} number={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} handleSave={handleSave} />
       <h2>Numbers</h2>
-      <Persons filteredPersons={filteredPersons} deletePerson={deletePerson} />
+      <Persons filteredPersons={filteredPersons} deletePerson={deletePerson}/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
